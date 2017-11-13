817c1dbfbb3dfe09cf77d97b8ea6e181c10daf71
Aaron Coburn <acoburn@amherst.edu>
2017-11-13 09:59:06 -0500

    Fix syntax

--- src/main/java/org/trellisldp/rosid/common/Namespaces.java
+++ src/main/java/org/trellisldp/rosid/common/Namespaces.java
@@ -137,7 +137,7 @@ public class Namespaces implements NamespaceService {
                 LOGGER.warn("Unable to read provided file: {}", ex.getMessage());
             }
         }
-        readTree(cache.getCurrentChildren(ZNODE_NAMESPACES)).forEachOrdered(namespaces::put);
+        readTree(cache.getCurrentChildren(ZNODE_NAMESPACES)).forEach(namespaces::put);
         return namespaces;
     }
 
